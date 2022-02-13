82f6512b718bbc199cf4414d01b775dacb30b635
Ryan <rtcondon@protonmail.com>
2022-02-13 14:55:54 -0800

    test

--- regexreplace.py
+++ regexreplace.py
@@ -1,26 +1,23 @@
 import re
 from bs4 import BeautifulSoup
 
-x = '''
-</main>
-<footer>
-<p><a href="#top">^Top</a> | <a href="index.html">Blog</a> - <a href="about.html">About</a></p><p> Cascading Style Sheet <a href="https://simplecss.org">simple.css</a> and HTML inspiration from the <a href="https://simplecss.org/showcase">showcase</a>. Cursor animation from <a href="https://www.amitmerchant.com/simple-blinking-cursor-animation-using-css/">Amit Merchant</a> Thank you all! </p>
-</footer>
-</body>
-</html>
-'''
 
-repltxt = '''
-<p><a href="#top">^Top</a> | <a href="index.html">Blog</a> - <a href="about.html">About</a></p><p> Cascading Style Sheet <a href="https://simplecss.org">simple.css</a> and HTML inspiration from the <a href="https://simplecss.org/showcase">showcase</a>. Cursor animation from <a href="https://www.amitmerchant.com/simple-blinking-cursor-animation-using-css/">Amit Merchant</a> Thank you all!!! </p>
-'''
+footer_file = open("../homepage/footer.html", 'r')
+#print(footer_file.read())
+
+footer_str = footer_file.read()
+
+repltxt = 'test'
 
 pattern = r'<footer>\n(.*?)\n</footer>'
-y = re.findall(r'<footer>\n(.*?)\n</footer>', x)[0]
+
+y = re.findall(r'<footer>\n(.*?)\n</footer>', footer_str)
 print(y)
 
-y = re.sub(pattern, repltxt, x)
+#y = re.sub(pattern, repltxt, footer_str)
+#print(y)
 
-soup = BeautifulSoup(y)
-prettyHTML = soup.prettify()
+#soup = BeautifulSoup(y)
+#prettyHTML = soup.prettify()
 
-print(prettyHTML)
+#print(prettyHTML)
